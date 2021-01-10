545884634ece794f777ed3d3afa8b8676624dda9
Przemyslaw Skrzypczynski <przemekskrzypczynski4@wp.pl>
2021-01-10 19:08:49 +0100

    add css styles

--- calendar.php
+++ calendar.php
@@ -13,24 +13,31 @@
         <meta charset="utf-8" />
         <title>Kalendarz</title>
 
-        <link rel="stylesheet" href="style.css" />
+        <style>
+        <?php
+            include_once "style.css";
+        ?>
+        </style>
     </head>
 
     <body>
 
         <?php
 
-        echo "<p>Witaj, ".$_SESSION['nickname'].'! [ <a href="logout.php">Wyloguj się!</a> ]</p>';
+        echo "<p>Witaj, ".$_SESSION['nickname'].'! [ <a class="link" href="logout.php">Wyloguj się!</a> ]</p>';
 
         ?>
+        
+        <div class="pageContent">
+            <div class="calendarNotesForm">
+                <textarea cols="40" rows="25" class="note">Dodaj swoją notatkę</textarea> 
+                <br />
+                <input type="button" value="Dodaj notatkę"/>
+            </div>
 
-        <form action="#" class="calendarNotesForm">
-            <textarea cols="40" rows="25">Dodaj swoją notatkę</textarea>
-            <input type="submit" value="Dodaj notatkę"/>
-        </form>
-
-        <div class="calendar-contener"></div>
-
+            <div class="calendar-contener"></div>
+        <div>
+        
     <script src="calendar.js"></script>
     </body>
 </html>
